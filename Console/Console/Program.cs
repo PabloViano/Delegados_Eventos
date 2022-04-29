e7f0eced0cd2a893a223b0a3f504d070dd04ce33
Pablo_Viano <pabloviano1@gmail.com>
2022-04-29 13:04:06 -0300

    Punto 1-a hecho

--- Console/Console/Program.cs
+++ Console/Console/Program.cs
@@ -1,40 +1,13 @@
-﻿public class NombreEventArgs : EventArgs
-{
-    public string Anterior { get; set; }
-    public string Actual { get; set; }
-}
+﻿using Logica;
 class Program
 {
     public delegate void actualizacionNombreCompletado(string nombre);
     static void Main(String[] args)
     {
         Persona persona = new Persona();
-        persona.NombreModificado += ActualizacionNombreCompletado;
+        persona.NombreModificado += Logger.ImprimirEnPantalla;
         persona.ActualizarNombre("Pablo");
-    }
-    static void ActualizacionNombreCompletado (object sender, NombreEventArgs e)
-    {
-        Console.Write($"El nombre fue modificado de {e.Anterior} a {e.Actual}");
-    }
-}
-public class Persona
-{
-    private string Nombre { get; set; }
-    public EventHandler<NombreEventArgs> NombreModificado;
-    public void ActualizarNombre(string nombre)
-    {
-        string anterior = this.Nombre;
-        this.Nombre = nombre;
-        if (this.NombreModificado != null)
-        {
-            this.NombreModificado(this, new NombreEventArgs { Actual = nombre, Anterior = anterior }); ;
-        }
-    }
-}
-public class Logger
-{
-    public void ImprimirEnPantalla()
-    {
 
+        Console.Read();
     }
 }
